8aff695912c07d969ec09e9063bb106083d712ae
lxc <1951381703@qq.com>
2024-01-06 18:54:22 +0800

    study

--- helloplus.py
+++ helloplus.py
@@ -4,11 +4,11 @@ app = flask.Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Hello, World</h1>'
+    return '<h1>Hello, World!</h1>'
 
 @app.route('/user/<name>')
 def user(name):
-    return f''
+    return f'<h1>Hello, {name}!</h1>'
 
 if __name__ == '__main__':
     app.run(debug=True)
